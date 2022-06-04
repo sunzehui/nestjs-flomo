a7d3a3abe0a4da8630db13e9b83d4e26cada4662
阿辉 <oncwnuEXKikeT96xJEFPh9GH37UM@git.weixin.qq.com>
2022-06-04 11:10:06 +0800

    'chore(readme):添加readme'

--- src/@types/pino-std-serializers.d.ts
+++
@@ -1,3 +0,0 @@
-interface Requestr {
-  name: string;
-}
--- test.ts
+++
@@ -1,3 +0,0 @@
-import * as moment from 'moment';
-const updateTime = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-console.log(updateTime);
