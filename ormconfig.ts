294d1fd9d62df3122a07d309fda3220db19c77d4
sunzehui <501574022@qq.com>
2022-09-11 06:33:51 +0800

    chore: 添加预览模式

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,30 @@
-import { DataSourceOptions } from 'typeorm';
-const ormCfg = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '123456',
-  database: 'flomo',
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
-  datestrings: true,
-} as DataSourceOptions;
+import {DataSourceOptions} from 'typeorm';
+import 'dotenv'
 
+let ormCfg = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'flomo',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    datestrings: true,
+} as DataSourceOptions;
+// 线上预览模式
+const isPreview = process.env.NODE_ENV;
+if (isPreview) {
+    ormCfg = {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'flomo',
+        password: 'flomo',
+        database: 'flomo',
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        datestrings: true,
+    } as DataSourceOptions;
+}
 export default ormCfg;
